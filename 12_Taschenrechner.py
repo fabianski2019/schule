00e9c109be80329d43f6357024137dd809fed2b8
fabianski2019 <86376072+fabianski2019@users.noreply.github.com>
2024-01-29 15:28:28 +0100

    Add files via upload

---
+++ 12_Taschenrechner.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+def button_click(symbol):
+    current.text= entry.get
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, current.text + str(symbol))
+
+def clear.entry
+    entry.delete(0. tk.END)
